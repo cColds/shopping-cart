6d5d0577e06217d4fc8e2f8b4451452b880839a5
cColds <103373668+cColds@users.noreply.github.com>
2023-06-27 20:20:53 -0500

    Add colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-color": "#2B5842",
+        "body-color": "#475569",
+      },
+    },
   },
   plugins: [],
 };
