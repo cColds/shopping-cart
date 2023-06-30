4d7d34f1af89195aab9b68e7130e03d1bb6d9aeb
cColds <103373668+cColds@users.noreply.github.com>
2023-06-30 17:33:18 -0500

    Add dynamic route to render item page

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/store" element={<Store />} />
+          <Route path="/store/:id" element={<div>Item page</div>} />
         </Routes>
       </main>
       <Footer />
