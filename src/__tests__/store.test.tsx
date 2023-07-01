eaa891086f6bbda40dbd99ba2e92040391d6ee53
cColds <103373668+cColds@users.noreply.github.com>
2023-06-30 20:14:41 -0500

    Refactor store tests

--- src/__tests__/store.test.tsx
+++ src/__tests__/store.test.tsx
@@ -1,44 +1,32 @@
 import "@testing-library/jest-dom";
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
 import Store from "../pages/Store";
 import mockData from "../mockData.json";
-it("should fetch mock.shop api", async () => {
-  (fetch as any).mockResponseOnce(JSON.stringify(mockData));
-
+it("items should render", () => {
+  const items = mockData.data.products.edges;
   render(
     <MemoryRouter>
-      <Store />
+      <Store items={items} />
     </MemoryRouter>
   );
-  expect(await screen.findByText(/slides/i)).toBeInTheDocument();
-  expect(await screen.findByText(/sweatpants/i)).toBeInTheDocument();
+  expect(screen.getByText(/slides/i)).toBeInTheDocument();
+  expect(screen.getByText(/sweatpants/i)).toBeInTheDocument();
 });
 
-it("should fail to fetch mock.shop api", async () => {
-  (fetch as any).mockRejectOnce(new Error("Failed to fetch!"));
-  render(
-    <MemoryRouter>
-      <Store />
-    </MemoryRouter>
-  );
-  expect(screen.queryByText(/slides/i)).not.toBeInTheDocument();
-  expect(screen.queryByText(/sweatpants/i)).not.toBeInTheDocument();
-});
+it("items in the store should have the correct link", () => {
+  const items = mockData.data.products.edges;
 
-it("items in the store should have the correct link", async () => {
-  (fetch as any).mockResponseOnce(JSON.stringify(mockData));
   render(
     <MemoryRouter>
-      <Store />
+      <Store items={items} />
     </MemoryRouter>
   );
-  await waitFor(() => {
-    const slides = screen.getByRole("link", { name: /slides for \$25/i });
-    const sweatpants = screen.getByRole("link", {
-      name: /sweatpants for \$35/i,
-    });
-    expect(slides).toHaveAttribute("href", "/store/slides");
-    expect(sweatpants).toHaveAttribute("href", "/store/sweatpants");
+
+  const slides = screen.getByRole("link", { name: /slides for \$25/i });
+  const sweatpants = screen.getByRole("link", {
+    name: /sweatpants for \$35/i,
   });
+  expect(slides).toHaveAttribute("href", "/store/slides");
+  expect(sweatpants).toHaveAttribute("href", "/store/sweatpants");
 });
