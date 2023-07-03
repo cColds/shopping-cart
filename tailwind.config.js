d015bce860147fb61800c4b7f3ced4cdc815969e
cColds <103373668+cColds@users.noreply.github.com>
2023-07-03 04:47:52 -0500

    Add custom modal bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,15 @@ export default {
       colors: {
         "primary-color": "#2B5842",
         "body-color": "#475569",
+        //  "modal": "rgba(255, 255, 255, 0.5)"
       },
       fontFamily: {
         "poppins-reg": ["Poppins", "system-ui"],
         "poppins-bold": ["Poppins-Bold", "system-ui"],
+      },
+
+      backgroundColor: {
+        modal: "rgba(0, 0, 0, 0.3)",
       },
     },
   },
