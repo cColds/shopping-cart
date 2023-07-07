ce4b09fd67585d01df49faff5af0d6843086c11b
cColds <103373668+cColds@users.noreply.github.com>
2023-07-07 01:48:12 -0500

    Fix featured items overflow

--- src/components/FeaturedItems.tsx
+++ src/components/FeaturedItems.tsx
@@ -3,7 +3,7 @@ import ItemCards from "./ItemCards";
 
 export default function FeaturedItems({ items }: { items: Item[] }) {
   return (
-    <section className="m-12 flex w-full flex-col items-center bg-slate-50 p-8 shadow-lg">
+    <section className="flex w-full flex-col items-center bg-slate-50 p-8 shadow-lg">
       <h2 className="text-center">Featured Items</h2>
       <ItemCards items={items} />
     </section>
