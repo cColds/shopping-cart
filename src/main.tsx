6d7cf83de640611924be3d9d15320c16acb61818
cColds <103373668+cColds@users.noreply.github.com>
2023-07-07 14:11:53 -0500

    Add basename "/shopping-cart" to MemoryRouter

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/shopping-cart">
       <App />
     </BrowserRouter>
   </React.StrictMode>
