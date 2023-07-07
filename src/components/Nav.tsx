dbfedd47c17bf6563da8d12c9f0a14b410ed7753
cColds <103373668+cColds@users.noreply.github.com>
2023-07-06 21:23:37 -0500

    Add responsive nav bar and change a links to link component

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,8 @@
 import logo from "../assets/logo.svg";
 import { Link } from "react-router-dom";
-import { FaSearch, FaShoppingCart } from "react-icons/fa";
+import { FaSearch, FaShoppingCart, FaHome, FaStore } from "react-icons/fa";
+import { MdMenu } from "react-icons/md";
+import { FaX } from "react-icons/fa6";
 import { useState, ChangeEvent } from "react";
 import Item from "../interfaces/Item";
 
@@ -16,6 +18,11 @@ export default function Nav({
   const [isSearchOpen, setIsSearchOpen] = useState(false);
   const [searchResults, setSearchResults] = useState<Item[]>([]);
   const [searchValue, setSearchValue] = useState("");
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuToggle = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   const handleSearchToggle = () => {
     if (!isSearchOpen) {
@@ -41,26 +48,55 @@ export default function Nav({
   };
 
   return (
-    <nav className="flex h-[60px] items-center justify-between gap-3 px-4 py-2	shadow-md">
+    <nav className="flex h-full flex-col items-center justify-between gap-3 px-4 py-2 shadow-md xs:h-[60px] xs:flex-row">
       <Link to="/" aria-label="home page" className="shrink-0">
         <img src={logo} alt="fashonique logo" className="h-[30px] w-[150px]" />
       </Link>
       <button
         type="button"
-        className="flex h-[40px] w-[300px] items-center gap-2 rounded-lg border-[1px] border-slate-200 focus:ring-sky-200"
+        className="hidden h-[40px] w-[300px] items-center gap-2 rounded-lg border-[1px] border-slate-200 sm:flex"
         onClick={handleSearchToggle}
       >
         <FaSearch className="m-2 mr-0 text-slate-500" aria-hidden="true" />
         <span className="m-2 ml-0 text-sm text-slate-500">Search</span>
       </button>
 
+      <ul className="flex gap-5">
+        <li className="flex items-center sm:hidden">
+          <button
+            type="button"
+            className="h-[40px] items-center justify-center rounded-lg"
+            onClick={handleSearchToggle}
+          >
+            <FaSearch className="m-2 mr-0 text-slate-500" aria-hidden="true" />
+          </button>
+        </li>
+        <li className="hidden items-center sm:flex">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="hidden items-center sm:flex">
+          <Link to="/store">Store</Link>
+        </li>
+        <li className="flex items-center">
+          <button className="relative" onClick={onCartToggle}>
+            <FaShoppingCart className="h-5 w-5" />
+            <span className="absolute right-[-10px] top-[-5px] flex h-[16px] min-w-[16px] items-center justify-center rounded-full bg-red-600 p-0.5 text-xs text-white">
+              {itemCount < 100 ? itemCount : 99 + "+"}
+            </span>
+          </button>
+        </li>
+
+        <li className="flex items-center sm:hidden">
+          <button className="relative" onClick={handleMenuToggle}>
+            <MdMenu className="h-6 w-6" />
+          </button>
+        </li>
+      </ul>
+
       <div
         data-search-modal={isSearchOpen}
         className="fixed right-0 top-0 z-50 flex h-full w-full items-center justify-center bg-modal backdrop-blur-sm data-[search-modal='false']:invisible"
         onClick={handleSearchToggle}
-        onKeyDown={(e) => {
-          if (e.key === "Escape") handleSearchToggle();
-        }}
       >
         <div
           className="flex h-[450px] w-[450px] flex-col rounded-lg bg-white"
@@ -93,12 +129,13 @@ export default function Nav({
               const itemUrl = title.replace(/\s+/g, "-").toLowerCase();
               return (
                 <li key={item.node.id} className="flex">
-                  <a
+                  <Link
                     className="flex h-[50px] flex-grow items-center rounded-lg border-b-[1px]  border-slate-200 bg-slate-100 p-2.5 text-sm"
-                    href={`/store/${itemUrl}`}
+                    to={`/store/${itemUrl}`}
+                    onClick={handleSearchToggle}
                   >
                     {title}
-                  </a>
+                  </Link>
                 </li>
               );
             })}
@@ -106,22 +143,67 @@ export default function Nav({
         </div>
       </div>
 
-      <ul className="flex gap-5">
-        <li className="flex items-center">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="flex items-center">
-          <Link to="/store">Store</Link>
-        </li>
-        <li className="flex">
-          <button className="relative" onClick={onCartToggle}>
-            <FaShoppingCart className="text-xl" />
-            <span className="absolute right-[-10px] top-[-5px] flex h-[16px] min-w-[16px] items-center justify-center rounded-full bg-red-600 p-0.5 text-xs text-white">
-              {itemCount < 100 ? itemCount : 99 + "+"}
-            </span>
-          </button>
-        </li>
-      </ul>
+      {isMenuOpen ? (
+        <div
+          data-modal={isMenuOpen}
+          className="fixed right-0 top-0 z-50 flex h-full w-full items-center justify-end bg-modal backdrop-blur-sm data-[modal='false']:invisible"
+          onClick={handleMenuToggle}
+        >
+          <div
+            className="flex h-full w-[250px] flex-col gap-5 bg-white p-4"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <div className="flex justify-between">
+              <Link to="/" aria-label="home page" className="shrink-0">
+                <img
+                  src={logo}
+                  alt="fashonique logo"
+                  className="h-[30px] w-[150px]"
+                />
+              </Link>
+              <button onClick={handleMenuToggle}>
+                <FaX />
+              </button>
+            </div>
+
+            <ul className="flex flex-col items-center gap-4 ">
+              <li className="flex w-full">
+                <Link
+                  to="/"
+                  className="flex w-full gap-5"
+                  onClick={handleMenuToggle}
+                >
+                  <FaHome className="h-6 w-6" />
+                  <span>Home</span>
+                </Link>
+              </li>
+              <li className="flex w-full">
+                <Link
+                  to="/store"
+                  className="flex w-full gap-5"
+                  onClick={handleMenuToggle}
+                >
+                  <FaStore className="h-6 w-6" />
+                  <span>Store</span>
+                </Link>
+              </li>
+
+              <li className="flex w-full">
+                <button
+                  className="flex w-full gap-5"
+                  onClick={() => {
+                    handleMenuToggle();
+                    onCartToggle();
+                  }}
+                >
+                  <FaShoppingCart className="h-6 w-6" />
+                  <span>Cart</span>
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      ) : null}
     </nav>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,20 @@ export default {
         "card-gray": "#e2e8f0",
       },
     },
+
+    screens: {
+      xs: "320px",
+
+      sm: "640px",
+
+      md: "768px",
+
+      lg: "1024px",
+
+      xl: "1280px",
+
+      "2xl": "1536px",
+    },
   },
   plugins: [],
 };
